683f3c0230df2bba43393247538ea2cf4ff68810
Mislav Žnidarec <mznidarec@omega-software.hr>
2022-09-20 15:02:22 +0200

    18

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from "@angular/core/src/metadata";
 
+interface Child {
+  name: string,
+  age: number
+}
+
 interface Passenger {
   id: number,
   fullname: string,
   checkedIn: boolean,
-  checkedInDate: number | null
+  checkedInDate: number | null,
+  children: Child[] | null
 }
 
 @Component({
@@ -20,9 +26,12 @@ interface Passenger {
               [class.checked-in]="passenger.checkedIn">
             </span>
             {{ i }}: {{ passenger.fullname }}
-            <p>{{ passenger | json}} </p>
             <div class="date">
-              Check in date: {{passenger.checkedInDate ? (passenger.checkedInDate | date: 'yMMMMd' | uppercase) : 'Not checked in'}}
+              Check in date:
+              {{passenger.checkedInDate ? (passenger.checkedInDate | date: 'yMMMMd' | uppercase) : 'Not checked in'}}
+            </div>
+            <div class="children">
+              Children: {{ passenger.children?.length || 0 }}
             </div>
           </li>
         </ul>
@@ -34,26 +43,31 @@ export class AppComponent{
     id: 1,
     fullname: 'Stephen',
     checkedIn: true,
-    checkedInDate: 1490742000000
+    checkedInDate: 1490742000000,
+    children: null
   }, {
     id: 2,
     fullname: 'Rose',
     checkedIn: false,
-    checkedInDate: null
+    checkedInDate: null,
+    children: [{name: 'Ted', age: 12}, {name: 'Chloe', age: 7}]
   }, {
     id: 3,
     fullname: 'James',
     checkedIn: true,
-    checkedInDate: 1491606000000
+    checkedInDate: 1491606000000,
+    children: null
   }, {
     id: 4,
     fullname: 'Louise',
     checkedIn: true,
-    checkedInDate: 1488412800000
+    checkedInDate: 1488412800000,
+    children: [{name: 'Jessica', age: 1}]
   }, {
     id: 5,
     fullname: 'Tina',
     checkedIn: false,
-    checkedInDate: null
+    checkedInDate: null,
+    children: null
   }];
 }
