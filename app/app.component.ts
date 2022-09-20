79cc6803478022e4112950d41099c8076a8edbd8
Mislav Žnidarec <mznidarec@omega-software.hr>
2022-09-20 10:21:04 +0200

    video 5

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core/src/metadata";
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
-    <div class="app">
-      Hello!
+    <div class = "app">
+      {{ title }}
     </div>
   `
 })
-export class AppComponent {
-
+export class AppComponent{
+  title: string;
+  constructor () {
+    this.title = 'Ultimate Angular';
+  }
 }
