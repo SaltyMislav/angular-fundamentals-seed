fbaceb3994532f1ece88f9ce470778c21dbedb02
Mislav Žnidarec <mznidarec@omega-software.hr>
2022-09-20 10:38:11 +0200

    9

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,11 +6,20 @@ import { Component } from "@angular/core/src/metadata";
   template: `
     <div class = "app">
       {{ title }}
+      <div>
+        {{ numberOne + numberTwo }}
+      </div>
+      <div>
+        {{ isHappy ? ':)' : ':(' }}
+      </div>
     </div>
   `
 })
 export class AppComponent{
   title: string;
+  isHappy: boolean = false;
+  numberOne: number = 1;
+  numberTwo: number = 2;
   constructor () {
     this.title = 'Ultimate Angular';
   }
