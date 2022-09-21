f9dc3d45da96ec18b462edf7a9d37afbb41d7055
Mislav Žnidarec <mznidarec@omega-software.hr>
2022-09-21 18:28:22 +0200

    51

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from "@angular/core/src/metadata";
 
+interface Nav{
+  link: string,
+  name: string,
+  exact: boolean
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
@@ -7,15 +13,11 @@ import { Component } from "@angular/core/src/metadata";
     <div class = "app">
       <nav class = "nav">
         <a
-          routerLink="/"
+          *ngFor="let item of nav"
+          [routerLink]="item.link"
           routerLinkActive="active"
-          [routerLinkActiveOptions]="{ exact: true }">
-          Home
-        </a>
-        <a
-          routerLink="/oops"
-          routerLinkActive="active">
-          404
+          [routerLinkActiveOptions]="{ exact: item.exact }">
+          {{ item.name }}
         </a>
       </nav>
       <router-outlet></router-outlet>
@@ -23,5 +25,16 @@ import { Component } from "@angular/core/src/metadata";
   `
 })
 export class AppComponent{
-
+  nav: Nav[] = [
+    {
+      link: '/',
+      name: 'Home',
+      exact: true
+    },
+    {
+      link: '/oops ',
+      name: '404',
+      exact: false
+    }
+  ];
 }
