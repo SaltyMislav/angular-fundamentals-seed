1f903b5f3c1045d9f9ea6830f6f93aaf9f344123
Mislav Žnidarec <mznidarec@omega-software.hr>
2022-09-21 12:54:08 +0200

    31

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { HttpModule } from '@angular/http'
 
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component'
 
@@ -17,6 +18,7 @@ import { PassengerDashboardService } from './passenger-dashboard.service'
   ],
   imports: [
     CommonModule,
+    HttpModule
   ],
   exports:[
     PassengerDashboardComponent
--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,7 +1,12 @@
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http/src";
+
 import { Passenger } from "./models/passenger.interface";
 
+@Injectable()
 export class PassengerDashboardService{
-  constructor() {}
+  constructor(private http: Http) {
+  }
 
   getPassengers(): Passenger[] {
     return [{
