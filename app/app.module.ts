305d4f89962aa9f7a1857bfbed878d9a2dc19f75
Mislav Žnidarec <mznidarec@omega-software.hr>
2022-09-21 18:00:31 +0200

    46

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core/src/metadata';
 import { BrowserModule } from '@angular/platform-browser'
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { PassengerDashBoardModule } from './passenger-dashboard/passenger-dashboard.module';
 
