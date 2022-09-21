c1b6fd6ff3a0dd3cfff69dd0b0a00aa011001d6c
Mislav Žnidarec <mznidarec@omega-software.hr>
2022-09-21 11:33:48 +0200

    27

--- app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, Output, EventEmitter } from '@angular/core'
 import { Passenger } from '../../models/passenger.interface'
 
 @Component({
@@ -10,7 +10,16 @@ import { Passenger } from '../../models/passenger.interface'
         class="status"
         [class.checked-in]="detail.checkedIn">
       </span>
-      {{ detail.fullname }}
+      <div *ngIf="editing">
+        <input
+          type="text"
+          [value]="detail.fullname"
+          (input)="onNameChange(name.value)"
+          #name>
+      </div>
+      <div *ngIf="!editing">
+        {{ detail.fullname }}
+      </div>
       <div class="date">
         Check in date:
         {{detail.checkedInDate ? (detail.checkedInDate | date: 'yMMMMd' | uppercase) : 'Not checked in'}}
@@ -18,13 +27,43 @@ import { Passenger } from '../../models/passenger.interface'
       <div class="children">
         Children: {{ detail.children?.length || 0 }}
       </div>
+      <button (click)="toggleEdit()">
+        {{ editing ? 'Done' : 'Edit'}}
+      </button>
+      <button (click)="onRemove()">
+        Remove
+      </button>
     </div>
   `
 })
 export class PassengerDetailComponent{
+
   @Input()
   detail: Passenger;
-  constructor(){
 
+  @Output()
+  edit: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  remove: EventEmitter<any> = new EventEmitter();
+
+  editing: boolean = false;
+
+  constructor(){
+  }
+
+  onNameChange (value: string){
+    this.detail.fullname = value;
+  }
+
+  toggleEdit() {
+    if(this.editing){
+      this.edit.emit(this.detail);
+    }
+    this.editing = !this.editing;
+  }
+
+  onRemove() {
+    this.remove.emit(this.detail);
   }
 }
--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -12,7 +12,9 @@ import { Passenger } from '../../models/passenger.interface';
       </passenger-count>
       <passenger-detail
         *ngFor="let passenger of passengers;"
-        [detail]="passenger">
+        [detail]="passenger"
+        (edit)="handleEdit($event)"
+        (remove)="handleRemove($event)">
       </passenger-detail>
     </div>
   `
@@ -53,5 +55,11 @@ export class PassengerDashboardComponent implements OnInit{
       checkedInDate: null,
       children: null
     }];
+  }
+  handleEdit(event) {
+    console.log(event);
+  }
+  handleRemove(event) {
+    console.log(event);
   }
 }
