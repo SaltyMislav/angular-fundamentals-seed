1c09aac3b41aa411664f0bb8f4d226407c1ba50b
Mislav Žnidarec <mznidarec@omega-software.hr>
2022-09-21 18:07:55 +0200

    47

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core/src/metadata';
 import { BrowserModule } from '@angular/platform-browser'
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { PassengerDashBoardModule } from './passenger-dashboard/passenger-dashboard.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     //angular modules
     BrowserModule,
     CommonModule,
+    RouterModule.forRoot(routes),
     //custom modules
     PassengerDashBoardModule
   ],
