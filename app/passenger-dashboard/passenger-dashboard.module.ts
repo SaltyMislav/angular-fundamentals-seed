b667ade1d1157b031dfc6afee31d25ae4de3467c
Mislav Žnidarec <mznidarec@omega-software.hr>
2022-09-21 18:36:24 +0200

    54

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -17,7 +17,14 @@ import { PassengerDashboardService } from './passenger-dashboard.service'
 const routes: Routes = [
   {
     path: 'passengers',
-    component: PassengerDashboardComponent
+    children: [
+      {
+        path: '', component: PassengerDashboardComponent
+      },
+      {
+        path: ':id', component: PassengerViewerComponent
+      }
+    ]
   }
 ];
 
